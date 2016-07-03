8f0913fdecd1c2ef236ee960b3c9cd1b8c683f38
Felipe <pipoblakgamer@gmail.com>
2016-07-03 19:26:25 -0300

    Settings com Cara melhor

--- app/src/main/java/app/com/example/pipob/popularmoviesapp/MainActivity.java
+++ app/src/main/java/app/com/example/pipob/popularmoviesapp/MainActivity.java
@@ -77,7 +77,7 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle item selection
+
         switch (item.getItemId()) {
             case R.id.action_settings:
                 startActivity(new Intent(this, SettingsActivity.class));
